9e5532b7074d9d59750c6981a3579af4f252ea38
Albrecht Gebhardt <albrecht.gebhardt@uni-klu.ac.at>
2007-04-07 00:00:00 +0000

    version 1.0-21

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,5 @@
 .First.lib <- function(lib, pkg) {
   library.dynam("sgeostat", pkg, lib)
-  if(version$major<="1" && version$minor<"9.0")
-    require(mva)
-  else
-    require(stats)
+  require(stats)
 }
   
